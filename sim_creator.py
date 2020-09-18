454e83e432c9843a033ddd615a1a5eadf74bf5a9
Santhosh Raghul <gssanthoshraghul1010@gmail.com>
2020-09-18 21:02:29 +0530

    update print statement

--- sim_creator.py
+++ sim_creator.py
@@ -1,7 +1,7 @@
 from functions import create_simfile
 
 if __name__ == "__main__":
-	print("Please use + for or, . for and, ! for not and () for brackets and single alphabets for variables\n")
+	print("Please use + for or, . for and, ! for not and () for braces only. Also please use single character variables\n")
 	exp=input("Enter expression : ")
 	filename=input("Enter filename : ")
 	create_simfile(exp,filename)
